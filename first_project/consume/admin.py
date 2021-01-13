aa0a1f5e4635977c3a27e58ff4191c3dcb98fd8e
[0cddo] <shim900516@gmail.com>
2021-01-13 18:40:42 +0900

    first_project

--- first_project/consume/admin.py
+++ first_project/consume/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from consume.models import Consumption
+
 # Register your models here.
+
+class ConsumptionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'buy_date', 'gender', 'age', 'product', 'total']
+
+admin.site.register(Consumption, ConsumptionAdmin)
--- first_project/consume/models.py
+++ first_project/consume/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Consumption(models.Model):
+    buy_date = models.CharField(max_length=20, null=True)
+    gender = models.CharField(max_length=10, null=True)
+    age = models.CharField(max_length=10, null=True)
+    product = models.CharField(max_length=50, null=True)
+    total = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return self.buy_date
