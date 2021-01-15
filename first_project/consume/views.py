c1e88362fc7ad14bd646e73e52eb4e614072cd01
[0cddo] <shim900516@gmail.com>
2021-01-15 23:39:52 +0900

    first_project

--- first_project/consume/views.py
+++ first_project/consume/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.base import View, TemplateView
+
+from django.http import HttpResponse, JsonResponse
+
+from django.core import serializers
+
+from .models import Consumption
 
 # Create your views here.
 
-# 내일 할일 views.py, urls.py 연결하기 
+class 
--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from.views import HomeView
+from consume.views import ConsumptionLV, ConsumptionDV
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('admin/', admin.site.urls),
+
+    path('consume/', include('consume.urls')),
+
 ]
